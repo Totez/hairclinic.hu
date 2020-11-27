6cd266763f1148fae463596e777ec1086e23bcc5
Katona Gergely <katonagergely@Totez-MBP-5.lan>
2020-11-27 12:32:51 +0100

    zsidro tanácsai responsive

--- resources/views/advice-of-zsidro.blade.php
+++ resources/views/advice-of-zsidro.blade.php
@@ -7,39 +7,40 @@
 		<img src="{{ asset("/images/header/HC_logo_png_feher.png") }}">
 	</div>
 	
-	<section is="advice-of-zsidro">
+	<section id="advice-of-zsidro">
 
 		<div class="container">
 
 			<div class="row">
 
-				<div class="col-11 col-md-8 bg-white mx-auto py-5 position-relative" style="margin-top:250px;padding-bottom:100px;margin-bottom:200px;">
+				<div class="col-11 col-md-12 col-xl-8 bg-white mx-auto py-5 position-relative" style="margin-top:250px;padding-bottom:100px;margin-bottom:200px;">
 
-                    <div class="row align-items-center">
-                    	<div class="col-6">
-                    		<p class="text-green text-sm" style="padding:3rem 7rem 3rem 7rem">
-                    			{!! $page_contents["advice"]['headline']['content'] !!}
-                    		</p>
+                    <div class="row align-items-center headline">
+                    	<div class="col-12 col-md-6">
+                            <p class="text-green text-sm text">
+                                {!! $page_contents["advice"]['headline']['content'] !!}
+                            </p>
                     	</div>
-                    	<div class="col-6">
-                            <img src="{{ asset($page_images["advice"]["headline"]["path"]) }}" alt="{{ $page_images["advice"]["headline"]["name"] }}" style="width:397px;margin-top:-400px;">
+                    	<div class="col-12 col-md-6 image order-first order-md-last">
+                            <img src="{{ asset($page_images["advice"]["headline"]["path"]) }}" alt="{{ $page_images["advice"]["headline"]["name"] }}">
                     	</div>
                     </div>
 
-                    <div class="row align-items-center" style="margin-bottom:50px;">
+                    <div class="row align-items-center title">
                     	<div class="col-12 text-center" style="height:200px;">
-                			<span class="w-100" style="font-size:67.3px;margin-right:-150px;line-height: 1.2;position:absolute;display: block;font-weight:400;padding-left:7px;">
+                			<span class="w-100 text">
 		                        {!! $page_contents["advice"]["description"]["content"] !!}
                 			</span>
                     	</div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-4">
-                            <img src="{{ asset($page_images["advice"]["advice"]["a1"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a1"]["name"] }}" style="width:123%;max-width:397px;max-height:397px;object-fit:contain;margin-left:-200px;">
+                    {{-- Ne mosd túl sokat a hajadat! --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-4 image">
+                            <img src="{{ asset($page_images["advice"]["advice"]["a1"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a1"]["name"] }}">
                         </div>
-                        <div class="col-8">
-                            <h3 class="mb-4" style="padding-right:7rem;">
+                        <div class="col-12 col-md-6 col-lg-8 description">
+                            <h3 class="mb-4">
                                 {{ $page_contents["advice"]["advice"]["a1"]['title'] }}
                             </h3>
                             <p class="text-green text-lg" style="font-style:italic;padding-right:7rem;">
@@ -49,8 +50,9 @@
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-7">
+                    {{-- Ügyelj arra, hogy mindig alaposan ki kell öblíteni a habot! --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-7 description">
                             <h3 class="mb-4" style="padding-left:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a2"]['title'] }}
                             </h3>
@@ -58,16 +60,17 @@
                                 {{ $page_contents["advice"]["advice"]["a2"]['content'] }}
                             </p>
                         </div>
-                        <div class="col-5 text-left pl-5">
-                            <img src="{{ asset($page_images["advice"]["advice"]["a2"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a2"]["name"] }}" style="width:100%;max-width:397px;max-height:397px;object-fit:contain;">
+                        <div class="col-12 col-md-6 col-lg-5 text-left image order-first order-md-last" style="padding-left:3rem;">
+                            <img src="{{ asset($page_images["advice"]["advice"]["a2"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a2"]["name"] }}" style="width:100%;max-width:397px;max-height:397px;object-fit:contain;margin-left:unset;">
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-5">
+                    {{-- Hagyd, hogy magától száradjon meg! --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-5 image">
                             <img src="{{ asset($page_images["advice"]["advice"]["a3"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a3"]["name"] }}" style="width:123%;max-width:480px;max-height:480px;object-fit:contain;margin-left:-160px;">
                         </div>
-                        <div class="col-7">
+                        <div class="col-12 col-md-6 col-lg-7 description">
                             <h3 class="mb-4" style="padding-right:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a3"]['title'] }}
                             </h3>
@@ -77,8 +80,9 @@
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-8">
+                    {{-- Csak jó minőségű kefével gondozd! --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-8 description">
                             <h3 class="mb-4" style="padding-left:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a4"]['title'] }}
                             </h3>
@@ -86,28 +90,29 @@
                                 {{ $page_contents["advice"]["advice"]["a4"]['content'] }}
                             </p>
                         </div>
-                        <div class="col-4 text-right">
-                            <img src="{{ asset($page_images["advice"]["advice"]["a4"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a4"]["name"] }}" style="width:123%;max-width:397px;max-height:397px;object-fit:contain;margin-right:-200px;">
+                        <div class="col-12 col-md-6 col-lg-4 text-right image order-first order-md-last">
+                            <img src="{{ asset($page_images["advice"]["advice"]["a4"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a4"]["name"] }}" style="margin-right:-200px;">
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-5">
+                    {{-- Hagyd pihenni a hajad! --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-5 image">
                             <img src="{{ asset($page_images["advice"]["advice"]["a5"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a5"]["name"] }}" style="width:100%;max-width:397px;max-height:397px;object-fit:contain;">
                         </div>
-                        <div class="col-7">
+                        <div class="col-12 col-md-6 col-lg-7 description">
                             <h3 class="mb-4" style="padding-right:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a5"]['title'] }}
                             </h3>
                             <p class="text-green text-lg" style="font-style:italic;padding-right:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a5"]['content'] }}
                             </p>
-                            
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-8">
+                    {{-- A diéta árthat a hajadnak --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-8 description">
                             <h3 class="mb-4" style="padding-left:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a6"]['title'] }}
                             </h3>
@@ -115,16 +120,17 @@
                                 {{ $page_contents["advice"]["advice"]["a6"]['content'] }}
                             </p>
                         </div>
-                        <div class="col-4 text-right">
-                            <img src="{{ asset($page_images["advice"]["advice"]["a6"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a6"]["name"] }}" style="width:123%;max-width:397px;max-height:397px;object-fit:contain;margin-right:-200px;">
+                        <div class="col-12 col-md-6 col-lg-4 text-right image order-first order-md-last">
+                            <img src="{{ asset($page_images["advice"]["advice"]["a6"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a6"]["name"] }}" style="margin-right:-200px;">
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                        <div class="col-5">
+                    {{-- Nyáron óvd a fejbőrödet --}}
+                    <div class="row align-items-center advice">
+                        <div class="col-12 col-md-6 col-lg-5 image">
                             <img src="{{ asset($page_images["advice"]["advice"]["a7"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a7"]["name"] }}" style="width:100%;max-width:397px;max-height:397px;object-fit:contain;">
                         </div>
-                        <div class="col-7">
+                        <div class="col-12 col-md-6 col-lg-7 description">
                             <h3 class="mb-4" style="padding-right:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a7"]['title'] }}
                             </h3>
@@ -135,8 +141,9 @@
                         </div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
-                    	<div class="col-7">
+                    {{-- Heti egy masszázs segíthet --}}
+                    <div class="row align-items-center advice">
+                    	<div class="col-12 col-md-6 col-lg-7 description">
                             <h3 class="mb-4" style="padding-left:7rem;">
                                 {{ $page_contents["advice"]["advice"]["a8"]['title'] }}
                             </h3>
@@ -144,12 +151,12 @@
                             	{{ $page_contents["advice"]["advice"]["a8"]['content'] }}
                     		</p>
                     	</div>
-                    	<div class="col-5 text-right">
+                    	<div class="col-12 col-md-6 col-lg-5 text-right image order-first order-md-last">
                             <img src="{{ asset($page_images["advice"]["advice"]["a8"]["path"]) }}" alt="{{ $page_images["advice"]["advice"]["a8"]["name"] }}" style="width:123%;max-width:480px;max-height:480px;object-fit:contain;margin-right:-160px;">
                     	</div>
                     </div>
 
-                    <div class="row align-items-center" style="margin:150px 0;">
+                    <div class="row align-items-center advice">
                         <div class="col text-center">
                             <h3>
                                 {{ $page_contents["advice"]['footnote']['title'] }}
